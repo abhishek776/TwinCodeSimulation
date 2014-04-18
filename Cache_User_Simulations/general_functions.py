63d78934f5d6ba1f3f9083b64ca93ce07095bd76
Abhishek Fatehpuria <abhishekfatehpuria776@gmail.com>
2014-04-18 13:48:35 -0700

    New Simulations

--- Cache_User_Simulations/general_functions.py
+++ Cache_User_Simulations/general_functions.py
@@ -4,11 +4,12 @@ def random_bandwith():
 	# interm = int(random() * 10)
 	# if random == 0:
 	# 	return 0.1
-	return random() + 0.5
+	# return random() + 0.5
 	# if int(random()*10) == 0:
 	# 	return 0.1
 	# else:
 	# 	return 1
+	return 1
 
 def random_int(total_num):
 	return int(random()*total_num)
@@ -16,7 +17,7 @@ def random_int(total_num):
 
 def random_color():
 	interm = int(random() * 4)
-	if interm < 3:
+	if interm < 2:
 		return 0
 	else:
 		return 1
